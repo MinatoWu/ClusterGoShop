ec96096da52bf88203782b5f476f931254607974
wiggins <wiggins.wu@anker-in.com>
2025-03-01 23:45:42 +0800

    test

--- mxshop_srvs/user_srv/model/user.go
+++ mxshop_srvs/user_srv/model/user.go
@@ -21,12 +21,13 @@ type BaseModel struct {
  3. md5 信息摘要算法
     密码如果不可以反解，用户找回密码
 */
+//用户表
 type User struct {
-	BaseModel
-	Mobile   string     `gorm:"index:idx_mobile;unique;type:varchar(11);not null"`
-	Password string     `gorm:"type:varchar(100);not null"`
-	NickName string     `gorm:"type:varchar(20)"`
-	Birthday *time.Time `gorm:"type:datetime"`
-	Gender   string     `gorm:"column:gender;default:male;type:varchar(6) comment 'female表示女, male表示男'"`
-	Role     int        `gorm:"column:role;default:1;type:int comment '1表示普通用户, 2表示管理员'"`
+	BaseModel            //基础参数
+	Mobile    string     `gorm:"index:idx_mobile;unique;type:varchar(11);not null"`
+	Password  string     `gorm:"type:varchar(100);not null"`
+	NickName  string     `gorm:"type:varchar(20)"`
+	Birthday  *time.Time `gorm:"type:datetime"`
+	Gender    string     `gorm:"column:gender;default:male;type:varchar(6) comment 'female表示女, male表示男'"`
+	Role      int        `gorm:"column:role;default:1;type:int comment '1表示普通用户, 2表示管理员'"`
 }
